f778874c6aef41cd5470071f0e2a1c451c9565c4
Administrator <Administrator@USERCHI-50GA8KV>
2017-12-23 20:15:38 +0800

    前端列表筛选页面已经完成.....$$$$$$$

--- src/main/java/com/skstravel/service/impl/MatcheServiceImpl.java
+++ src/main/java/com/skstravel/service/impl/MatcheServiceImpl.java
@@ -17,11 +17,11 @@ public class MatcheServiceImpl implements MatcheService {
 	 private  JdbcTemplate jdbcTemplateForSksports2; 
 
 	public List<Map<String, Object>> findMatchListForIndex() {
-		
-		String sql="SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress ,sp.big_pitch_img "
-				+ "matchImg ,FROM_UNIXTIME(sg.add_time,'%Y-%m-%d %h:%i:%s')  matchTime,"
-				+ "sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  "
-				+ "WHERE  sp.region_id =sr.region_id AND sp.id=sg.pitch_id AND  sg.game_id='60'";
+		String sql=" SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress "
+					+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(ss.sche_start,'%Y-%m-%d %h:%i:%s')  matchTime, "
+					+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id ";
 	 	List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql);
 		return list;
 	}
@@ -34,23 +34,29 @@ public class MatcheServiceImpl implements MatcheService {
 	}
 
 	public List<Map<String, Object>> findRankList() {
-		String sql2="SELECT DISTINCT g.rank  FROM sk_goods g WHERE g.goods_sn LIKE 'FWC%' ORDER BY  g.rank ";
+		String sql2=" SELECT  DISTINCT sg.rank   FROM sk_goods sg, sk_pitch sp ,sk_region   sr " 
+					+ " ,sk_schedule ss  , sk_number sn "
+					+ " WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+ " AND sn.id=sg.number_id AND sn.pitch_id =sp.id ORDER BY sg.rank ";
      	List<Map<String, Object>> rankList = jdbcTemplateForSksports2.queryForList(sql2);
 		return rankList;
 	}
 
 	public List<Map<String, Object>> findCityList() {
-		String sql1="SELECT   p.id,  p.pitch_name ,r.region_name FROM sk_pitch p,sk_region "
-     			+ " r WHERE p.region_id=r.region_id  AND   p.pitch_sequence>79";
+		String sql1=" SELECT  DISTINCT   sp.id,  sp.pitch_name ,sr.region_name FROM sk_goods sg, sk_pitch sp ,sk_region   sr  "
+					+" ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id ";
      	List<Map<String, Object>> cityList = jdbcTemplateForSksports2.queryForList(sql1);
 		return cityList;
 	}
 
 	public List<Map<String, Object>> findByGameStage(int gameS) {
-		String sql="SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress"
-				+ " ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(sg.add_time,'%Y-%m-%d %h:%i:%s')  matchTime,"
-				+ "sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss "
-				+ "WHERE  sp.region_id =sr.region_id AND sp.id=sg.pitch_id AND ss.id= sg.sche_id AND ss.id= ? AND  sg.game_id='60' ";
+		String sql=" SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress "
+					+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(ss.sche_start,'%Y-%m-%d %h:%i:%s')  matchTime, "
+					+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=?  ";
     	List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameS);
 		return list;
 	}
@@ -59,10 +65,9 @@ public class MatcheServiceImpl implements MatcheService {
 	public List<Map<String, Object>> findCityByGameStage(int gameStage) {
 			//String valueOf = String.valueOf(gameStage);
 		
-		String sql=" SELECT DISTINCT sr.region_name  ,sr.region_id FROM sk_game sga,sk_schedule ss,sk_goods sgo ,sk_pitch sp,sk_region sr WHERE " 
-					+" sgo.game_id=sga.id AND sgo.sche_id =ss.id AND sga.id= ss.game_id AND sr.region_id=sp.region_id "
-					+" AND sgo.pitch_id=sp.id AND sga.id='60' "
-					+" AND ss.id=? ";
+		String sql=" SELECT DISTINCT sr.region_id, sr.region_name FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+"	AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=?  ";
 		List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameStage);
 		return list;
 	}
@@ -70,10 +75,9 @@ public class MatcheServiceImpl implements MatcheService {
 	@Override
 	public String findGameStageByPage(int gameStage) {
 		//String valueOf = String.valueOf(gameStage);
-		String sql=" SELECT DISTINCT ss.sche_name  FROM sk_game sga,sk_schedule ss,sk_goods sgo ,sk_pitch sp,sk_region sr WHERE " 
-				+" sgo.game_id=sga.id AND sgo.sche_id =ss.id AND sga.id= ss.game_id AND sr.region_id=sp.region_id "
-				+" AND sgo.pitch_id=sp.id AND sga.id='60' "
-				+" AND ss.id=? ";
+		String sql="  SELECT  DISTINCT ss.sche_name FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=? ";
 		List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameStage);
 		String str=null;
 		for (Map<String, Object> map : list) {
@@ -87,11 +91,11 @@ public class MatcheServiceImpl implements MatcheService {
 	 * 通过赛事和城市确定  要展示的列表
 	 */
 	public List<Map<String, Object>> findByGameStageAndCity(int gameStage, int city) {
-		String sql=" SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress"
-					+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(sg.add_time,'%Y-%m-%d %h:%i:%s')  matchTime, "
-					+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  "
-					+" WHERE  sp.region_id =sr.region_id AND sp.id=sg.pitch_id AND ss.id= sg.sche_id AND ss.id=?  AND  sg.game_id='60' "
-					 +" AND sr.region_id=? " ;
+		String sql="SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress "
+					+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(ss.sche_start,'%Y-%m-%d %h:%i:%s')  matchTime, "
+					+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=?  AND sr.region_id=?  " ;
 		List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameStage,city);
 		return list;
 	}
@@ -100,10 +104,9 @@ public class MatcheServiceImpl implements MatcheService {
 	 * 通过页面中发送的城市id查找到城市数据
 	 */
 	public String findCityByPage(int city) {
-		String sql=" SELECT DISTINCT  sr.region_name   FROM sk_game sga,sk_schedule ss,sk_goods sgo ,sk_pitch sp,sk_region sr WHERE " 
-						+" sgo.game_id=sga.id AND sgo.sche_id =ss.id AND sga.id= ss.game_id AND sr.region_id=sp.region_id" 
-						+" AND sgo.pitch_id=sp.id AND sga.id='60' "
-						+" AND  sr.region_id=? ";
+		String sql="   SELECT  DISTINCT  sr.region_name FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' " 
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND sr.region_id= ? ";
 		String str = null;
 		List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,city);
 		for (Map<String, Object> map : list) {
@@ -117,10 +120,9 @@ public class MatcheServiceImpl implements MatcheService {
 	 * 通过城市和赛事确定级别列表
 	 */
 	public List<Map<String, Object>> findRankByCityAndgameStage(int gameStage, int city) {
-		String sql=" SELECT DISTINCT sgo.rank rank ,sgo.goods_id id  FROM sk_game sga,sk_schedule ss,sk_goods sgo ,sk_pitch sp,sk_region sr WHERE " 
-				+" sgo.game_id=sga.id AND sgo.sche_id =ss.id AND sga.id= ss.game_id AND sr.region_id=sp.region_id "
-				+" AND sgo.pitch_id=sp.id AND sga.id='60' "
-				+" AND ss.id=?  and sr.region_id=?  group by sgo.rank   asc  ";
+		String sql="   SELECT  DISTINCT sg.rank rank ,sg.goods_id id FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+				+"	WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60'  "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=?  AND sr.region_id= ? GROUP BY sg.rank   ASC ";
 		List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameStage,city);
 		return list;
 	}
@@ -129,11 +131,11 @@ public class MatcheServiceImpl implements MatcheService {
 	 * 通过赛事和城市和级别获得展示列表
 	 */
 	public List<Map<String, Object>> findByGameStageAndCityAndRank(int gameStage1, int city1, int rank1) {
-		String sql=" SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress"
-				+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(sg.add_time,'%Y-%m-%d %h:%i:%s')  matchTime, "
-				+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  "
-				+" WHERE  sp.region_id =sr.region_id AND sp.id=sg.pitch_id AND ss.id= sg.sche_id AND ss.id=?  AND  sg.game_id='60' "
-				 +" AND sr.region_id=? and sg.goods_id=? " ;
+		String sql="  SELECT  sg.goods_id matchId ,sg.goods_name matchTitle ,sg.shop_price matchPrice ,sp.pitch_name  matchAddress "
+					+" ,sp.big_pitch_img matchImg ,FROM_UNIXTIME(ss.sche_start,'%Y-%m-%d %h:%i:%s')  matchTime, "
+					+" sg.ticket_business matchTxt FROM sk_goods sg, sk_pitch sp ,sk_region   sr  ,sk_schedule ss  , sk_number sn "
+					+" WHERE  sp.region_id =sr.region_id AND ss.id= sg.sche_id  AND  sg.game_id='60' "
+					+" AND sn.id=sg.number_id AND sn.pitch_id =sp.id AND ss.id=? AND sr.region_id=? AND    sg.goods_id= ? " ;
 	List<Map<String, Object>> list = jdbcTemplateForSksports2.queryForList(sql,gameStage1,city1,rank1);
 	return list;
 	}
@@ -142,7 +144,12 @@ public class MatcheServiceImpl implements MatcheService {
 	 * 查询选中的赛事的详情
 	 */
 	public void findDetailsById(int id) {
-		
+		 String sql="SELECT DISTINCT sg.goods_id  , scp.combo_pitch_id ,sp.pitch_name ,scp.combo_pitch_name ,combo_pitch_desc, "
+				 	+" sp.big_pitch_img FROM sk_combo_pitch scp , "
+				 	+" sk_combo sco ,sk_combo_ticket  sct ,sk_goods  sg,sk_pitch sp ,sk_number sn "
+				 	+" WHERE scp.combo_pitch_id =sco.combo_pitchs AND sco.combo_id= "
+				 	+" sct.combo_id AND sct.goods_id=sg.goods_id AND sg.game_id='60' AND "  
+				 	+" sg.number_id=sn.id AND sn.pitch_id= sp.id  AND sco.is_show='0' AND  sg.goods_id = ?   ";
 		
 	}
  
