fa733aea7750f215e2a53c27bb633552a02330a6
jeffery <jeffery@163.com>
2018-02-01 22:13:37 +0800

    修改注册实体类存空值问题

--- src/main/java/com/skstravel/service/impl/UserServiceImpl.java
+++ src/main/java/com/skstravel/service/impl/UserServiceImpl.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Service;
 
 import com.skstravel.service.UserService;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -43,11 +47,51 @@ public class UserServiceImpl implements UserService {
         criteria.andMobilePhoneEqualTo(mobilePhone);
         List<SkUsers> list = skUsersMapper.selectByExample(example);
         Integer i = 0;
-        if (list == null || list.size() == 0) {
-             skUsersMapper.insertSelective(skUsers);
+        if(list==null||list.size()==0){
+            skUsers.setEmail(" ");
+
+            skUsers.setQuestion(" ");
+            skUsers.setAnswer(" ");
+            skUsers.setSex(true);
+            skUsers.setUserMoney(new BigDecimal("0.0"));
+            skUsers.setFrozenMoney(new BigDecimal("0.0"));
+            skUsers.setPayPoints(0);
+            skUsers.setRankPoints(0);
+            skUsers.setAddressId(0);
+            skUsers.setRegTime(0);
+            skUsers.setLastLogin(0);
+            skUsers.setLastIp(" ");
+            skUsers.setVisitCount((short)0);
+            skUsers.setUserRank((byte)0);
+            skUsers.setIsSpecial((byte)0);
+            skUsers.setParentId(0);
+            skUsers.setFlag((byte)0);
+            skUsers.setIsValidated((byte)0);
+            skUsers.setCreditLine(new BigDecimal("0.0"));
+            skUsers.setWxOpenId(" ");
+            skUsers.setType((byte)0);
+            skUsers.setCreditsNum(0);
+            skUsers.setUsedCreditsNum(0);
+            skUsers.setSource(true);
+            skUsers.setLastIp(" ");
+            skUsers.setEcSalt(" ");
+            skUsers.setSalt(" ");
+            skUsers.setAlias(" ");
+            skUsers.setMsn(" ");
+            skUsers.setQq(" ");
+            skUsers.setOfficePhone(" ");
+            skUsers.setHomePhone(" ");
+            skUsers.setPasswdAnswer(" ");
+            skUsers.setPasswdAnswer(" ");
+            skUsers.setEmergencyPhone(" ");
+            skUsers.setIsThird(" ");
+            skUsers.setInUrt(" ");
+            skUsers.setAdress(" ");
+            skUsers.setCompanyName(" ");
+            skUsersMapper.insertSelective(skUsers);
             Integer userId = skUsers.getUserId();
             i=userId;
-        } else {
+        }else{
             return i;
         }
         return i;
